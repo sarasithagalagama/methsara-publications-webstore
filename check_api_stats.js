91226786f7f769860bddd7b6bd8e38a840ce4bbf
Sarasitha Galagama <sarasithagalagama@gmail.com>
2026-02-22 22:23:15 +0530

    chore: clean up root directory by removing test scripts and empty folders

--- check_api_stats.js
+++
@@ -1,7 +0,0 @@
-const axios = require("axios");
-require("dotenv").config();
-
-async function checkApiStats() {
-  const token = "YOUR_TOKEN_HERE"; // I don't have a token easily, I'll use a direct internal check
-}
-// Scratch that, I'll just run a script that simulates the controller logic in the app context.
